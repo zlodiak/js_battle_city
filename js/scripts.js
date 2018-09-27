c77d41762cb215eddff0f99f34f17929990e3e0e
kalinin <sergey.kalinin@simbirsoft.com>
2018-09-27 17:48:36 +0300

    info screen v1

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,34 @@ class Game {
 
   constructor(initObj) {
     this.gameEl = initObj.gameEl;
+    this.levelNum = null;    
     this.menu = new Menu({
-      gameEl: this.gameEl
+      gameEl: this.gameEl,
+      exitCallback: this.startGame
     });
   }
+
+  startGame() {
+    delete this.menu;
+    this.levelNum = 1;
+
+    // while (this.levelNum < 3) {
+      this.infoScreen = new infoScreen({
+        gameEl: this.gameEl,
+        topText: 'Уровень ' + this.levelNum,
+        bottomText: 'Нажмите любую клавишу для старта',
+        exitCallback: this.activationLevel
+      });
+    // }
+  }
+
+  activationLevel() {
+    alert('activationLevel')
+    // console.log('this.levelNum', this.levelNum)
+    // console.log('this.infoScreen', this.infoScreen)
+    // delete this.infoScreen;
+    // alert('level')
+  }  
 
 }
 
@@ -13,6 +37,7 @@ class Menu {
 
   constructor(initObj) {
     this.gameEl = initObj.gameEl;
+    this.exitCallback = initObj.exitCallback;
 
     this.enemiesCntIndex = 2;
     this.enemiesCntLabels = {
@@ -26,7 +51,9 @@ class Menu {
       0: 'Алмазные',
       1: 'Железобетонные',
       2: 'Кирпичные'
-    };    
+    };
+
+    this.infoScreen = null;
 
     this.render();
     this.startKeysListen();
@@ -60,11 +87,9 @@ class Menu {
     delete this._keysListenHandler;
   }
 
-  _keysListen(e) {
-    console.log(this)
+  _keysListen(e) {    
     switch(e.keyCode) {
       case 49:
-        console.log(this)
         ++this.enemiesCntIndex;
         if (this.enemiesCntIndex > 2) { this.enemiesCntIndex = 0; }
         this.render();
@@ -75,9 +100,56 @@ class Menu {
         this.render();
         break;  
       case 51:
-        this.stopKeysListen();
-        alert('start game');
+        this.stopKeysListen();  
+        this.exitCallback();            
     }     
+  }
+
+
+
+}
+
+
+class infoScreen {
+
+  constructor(initObj) {
+    console.log(initObj)
+    this.gameEl = initObj.gameEl;
+    this.topText = initObj.topText;
+    this.bottomText = initObj.bottomText;
+    this.exitCallback = initObj.exitCallback;
+
+    this.render();
+    document.addEventListener('keypress', () => {
+      console.log('close info screen');
+      this.destroy();
+    });
+  }
+
+  render() {
+    this.gameEl.innerHTML = '';
+
+    const infoScreenTpl = document.querySelector('#infoScreenTpl');
+    const infoScreenTplClone = infoScreenTpl.content.cloneNode(true);
+    this.gameEl.appendChild(infoScreenTplClone);
+
+    const topText = document.getElementById('infoTextTop');
+    if (topText) { topText.innerHTML = this.topText; }
+
+    const bottomText = document.getElementById('infoTextBottom');
+    if (bottomText) { bottomText.innerHTML = this.bottomText; }
+  }
+
+  destroy() {
+    const wrapInfoScreenEl = document.getElementById('wrapInfoScreen');    
+    if (wrapInfoScreenEl) {
+      wrapInfoScreenEl.classList += ' hide';
+    }
+
+    setTimeout(() => {
+      this.exitCallback();
+    }, 1000);
+    
   }
 
 }
