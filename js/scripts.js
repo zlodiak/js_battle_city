e256f68ff655e30173d8c805d23ac85c7ada5633
zlodiak <prozaik81-2@yandex.ru>
2018-09-30 15:50:11 +0300

    walls render ok

--- js/scripts.js
+++ js/scripts.js
@@ -55,9 +55,9 @@ class Menu {
 
     this.enemiesCntIndex = 2;
     this.enemiesCntLabels = {
-      0: 'Много',
+      0: 'Мало',
       1: 'Средне',
-      2: 'Мало'
+      2: 'Много'
     };
     
     this.wallsStrengthIndex = 2;
@@ -238,20 +238,29 @@ class Level {
     console.log(menuSettings)
     this.gameObj = gameObj;
     this.wallsStrengthIndex = menuSettings.wallsStrengthIndex;
-    this.wallsCnt = 10;
-    this.walls = [];
-    this.generateWalls();
+    this.wallsCnt = 30;
+    this.walls = [];    
     this.render();
   }
 
   generateWalls() {
-    for (let id = 0; id < 10; id++) {
-      const wall = new Wall(id, this.wallsStrengthIndex);
+    for (let id = 0; id < this.wallsCnt; id++) {
+      const wall = new Wall(this, this.gameObj.gameEl, id, this.wallsStrengthIndex);
+      this.walls.push(wall);
     }
+  }
+
+  deleteWall(id) {
+    this.walls.forEach((w, i) => {
+      if (this.walls[i] && this.walls[i].id === id) {
+        this.walls.splice(i, 1);
+      }      
+    })
   }
 
   render() {
     this.gameObj.gameEl.innerHTML = '';
+    this.generateWalls();
 
     // const next = document.createElement('div');
     // next.id = 'next';
@@ -278,17 +287,43 @@ class Level {
 
 class Wall {
 
-  constructor(id, wallsStrengthIndex) {
+  constructor(levelObj, fieldEl, id, wallsStrengthIndex) {
+    this.levelObj = levelObj;
+    this.fieldEl = fieldEl;
     this.id = id;
     this.xCoord = Math.floor(Math.random() * (10));
     this.yCoord = Math.floor(Math.random() * (10));
-    this.strength = wallsStrengthIndex;
-    console.log(id, this.xCoord, this.yCoord, this.strength)
+    this.strength = wallsStrengthIndex;    
+    this.size = 80;
+    switch(wallsStrengthIndex) {
+      case 0: this.background = 'red'; break;
+      case 1: this.background = 'grey'; break;
+      case 2: this.background = 'cyan'; break;                
+    }
     this.render();
   }
 
   render() {
+    const wallEl = document.createElement('div');
+    wallEl.id = 'wall_' + this.id;
+    wallEl.classList += 'wall';
+    wallEl.style.background = this.background;
+    wallEl.style.left = this.xCoord * this.size + 'px';
+    wallEl.style.top = this.yCoord * this.size + 'px';
+    this.fieldEl.appendChild(wallEl);
 
+    document.getElementById('wall_' + this.id).addEventListener('click', () => {
+     --this.strength;
+     if (this.strength < 0) { this.destroy(); }      
+    });
+  }
+
+  destroy() {
+    const wallEl = document.getElementById('wall_' + this.id);
+    if (wallEl) {
+      wallEl.remove();
+      this.levelObj.deleteWall(this.id);
+    }
   }
 
 }
